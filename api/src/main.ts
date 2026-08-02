05eab0181f656a99886fec155c922f932cb46720
Badr-Eddine-Bourghel <BOURGHEL.BADREDDINE@ensam-casa.ma>
2026-08-02 23:06:40 +0100

    Validation Pipes

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Project description
   app.setGlobalPrefix('api/v1');
+
+  // Set Global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap().catch((error) => {
